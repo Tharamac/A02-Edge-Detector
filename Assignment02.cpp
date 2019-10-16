093e6af4f2efef173e1d64a1a8b9a9bc3ca1e7cf
Macxiiz <tharathorn.wang@gmail.com>
2019-10-16 09:10:18 +0700

    Polish <Remove Noise>

--- Assignment02.cpp
+++ Assignment02.cpp
@@ -54,50 +54,61 @@ int main()
 	//convert to HSV
 	cvtColor(image, image_hsv, COLOR_BGR2HSV);
 
-	imshow("2.1 HSV Input", image_hsv); waitKey(0);
+	imshow("2. HSV Input", image_hsv); waitKey(0);
 	split(image_hsv, channel);
 	//convert to HSV channel[1]
-	imshow("2.2 GRAYSCALE Input", channel[1]); waitKey(0);
+	imshow("3. Saturation Channel", channel[1]); waitKey(0);
 	//create Mask form HSV cut the red out
 	Mat hsvMask;
 	inRange(image_hsv, Scalar(0, 0, 80), Scalar(255, 255, 255), hsvMask);
-	imshow("3.1 (2.1)RED OUT", hsvMask); waitKey(0);
+	imshow("4. RED OUT form (2)", hsvMask); waitKey(0);
 	//create Mask form HSV channel[1] <Saturation>
 	Mat vMask, mergeMask;
 	inRange(channel[1], Scalar(67, 67, 67), Scalar(255, 255, 255), vMask);
 	bitwise_not(vMask, vMask);
-	imshow("3.2 Channel Saturation Mask", vMask); waitKey(0);
+	imshow("5. BLACK OUT from (3)", vMask); waitKey(0);
 	//create Mask from light brown
 	inRange(image, lowBrown, highBrown, brownMask);
-	imshow("3.3 Brown Mask", brownMask); waitKey(0);
+	imshow("6. Reflected Brown Mask from (1)", brownMask); waitKey(0);
 
-	vector<Point> cut_start = { Point(0,0),Point(277, 478) , Point(34, 292),  Point( brownMask.cols / 2,0),Point(478, 365)};
-	vector<Point> cut_finish = { Point(419,303),Point(461, 585), Point(282, 581) ,Point(brownMask.cols, brownMask.rows),Point(575, 476)}; 
+	vector<Point> cut_start = { Point(0,0),Point(277, 478) , Point(0, 292),  Point( brownMask.cols / 2,0),Point(478, 365),Point(328, 515),	Point(436, 438) };
+	vector<Point> cut_finish = { Point(419,303),Point(461, 585), Point(282, 581) ,Point(brownMask.cols, brownMask.rows),Point(575, 476),Point(676, brownMask.rows),	Point(620, 533) };
+
+	/*
+ (806, 310) is (19, 27, 50)
+Left button of the mouse is clicked - position (937, 392)
+	*/
+	
 	cout << "COL" << brownMask.cols << "ROW" << brownMask.rows << endl;
 	for (int i = 0; i < cut_start.size(); i++) {
-		cout << i;
 		rectangle(brownMask, cut_start[i], cut_finish[i], Scalar(0), CV_FILLED);
 	}
 	
-	imshow("3.4 Brown Mask", brownMask); waitKey(0);
+	imshow("7. Cut Shadow <Black>", brownMask); waitKey(0);
 	
 	
 	//Combine two mask to a single mask
 	bitwise_and(hsvMask, vMask, mergeMask);
-	imshow("4.1 3.1) + 3.2)", mergeMask); waitKey(0);
+	imshow("8. (4) and (5)", mergeMask); waitKey(0);
 	bitwise_or(mergeMask, brownMask, mergeMask);
-	imshow("4.2 4.1) + 3.3)", mergeMask); waitKey(0);
+	imshow("9. (8) or (5)", mergeMask); waitKey(0);
+
+	vector<Point> white_start = { Point(0,0),Point(394, 510) , Point(111,0),Point(371, 525),Point(386, 518) ,Point(136, 355),Point(806, 310) };
+	vector<Point> white_finish = { Point(163, 43),Point(449, 544) ,Point(308,30),Point(402, 542),Point(407, 533) ,Point(0, 467) ,Point(937, 392) };
+	for (int i = 0; i < white_start.size(); i++) {
+		rectangle(mergeMask, white_start[i], white_finish[i], Scalar(255), CV_FILLED);
+	}
+	imshow("10. Cut Shadow <White>", mergeMask); waitKey(0);
 	
 	bitwise_xor(mergeMask, brownMask, mergeMask);
-	imshow("4.3 4.1) + 3.3)", mergeMask); waitKey(0);
+	imshow("11. (9) xor (10)", mergeMask); waitKey(0);
 	//close mask to remove line of tiles
-	/*
+	
 	Mat element = getStructuringElement(morph_elem, Size(2 * morph_size + 1, 2 * morph_size + 1), Point(morph_size, morph_size));
 	morphologyEx(mergeMask, mergeMask, MORPH_CLOSE, element);
-	morphologyEx(mergeMask, mergeMask, MORPH_OPEN, element);
-    imshow("5. Morph Close", mergeMask); waitKey(0);
-	*/
-	//
+	//morphologyEx(mergeMask, mergeMask, MORPH_OPEN, element);
+    imshow("12. Morph Close & Morph Open", mergeMask); waitKey(0);
+	
 	
 	Mat canny_output;
 	
@@ -117,12 +128,12 @@ int main()
 	findContours(canny_output, contours, hierarchy, RETR_TREE, CHAIN_APPROX_SIMPLE);
 	Mat drawing = Mat::zeros(canny_output.size(), CV_8UC3);
 	RNG rng(12345);
-
+	//Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
 	// drawContours
 	for (size_t i = 0; i < contours.size(); i++)
 	{
 		Scalar color = Scalar(rng.uniform(0, 256), rng.uniform(0, 256), rng.uniform(0, 256));
-		drawContours(drawing, contours, (int)i, Scalar(255,255,255), 2, LINE_8, hierarchy, 0);
+		drawContours(drawing, contours, (int)i, color, 2, LINE_8, hierarchy, 0);
 	}
 	// Wait until user press some key
 	imshow("Contours", drawing);
